552503c9d727e06f21a685550002c3a1817c9671
Fredrik Simonsson <simson@thesimson.net>
2022-11-13 19:45:19 +0900

    Update depenency

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ extern crate panic_halt;
 
 use core::ops;
 
+use deposit_iter::Deposit;
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
@@ -25,9 +27,9 @@ fn main() -> ! {
     let mut duty_1 = Duty::new();
     let mut duty_4 = Duty::new();
     loop {
-        let mut d0_pwm_duty = Deposit::new(duty_0.get());
-        let mut d1_pwm_duty = Deposit::new(duty_1.get());
-        let mut d4_pwm_duty = Deposit::new(duty_4.get());
+        let mut d0_pwm_duty = Deposit::new(duty_0.get().into());
+        let mut d1_pwm_duty = Deposit::new(duty_1.get().into());
+        let mut d4_pwm_duty = Deposit::new(duty_4.get().into());
         d0.set_high();
         d1.set_high();
         d4.set_high();
@@ -53,32 +55,6 @@ fn main() -> ! {
     }
 }
 #[derive(Default)]
-struct Deposit {
-    times: Option<u8>,
-}
-
-impl Deposit {
-    fn new(t: u8) -> Self {
-        Deposit { times: Some(t) }
-    }
-}
-impl Iterator for Deposit {
-    type Item = bool;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(times) = self.times {
-            if times == 0 {
-                self.times = None;
-                Some(true)
-            } else {
-                self.times = Some(times.saturating_sub(1));
-                Some(false)
-            }
-        } else {
-            None
-        }
-    }
-}
 
 struct Duty {
     internal: i32,
@@ -94,7 +70,6 @@ impl Duty {
         (self.internal.abs() as u32).to_le_bytes()[1] / 2
     }
 }
-
 impl ops::AddAssign<i32> for Duty {
     fn add_assign(&mut self, incr: i32) {
         if self.internal + incr > INTERNAL_MAX {
